e1e379bd44cc9f744d9c969ecab4e00cb19bda17
dbaranov-hodies <dmitrii.baranov@hoodies.team>
2026-02-09 13:06:35 +0400

    addressed second series of comments, created after formal approval

--- src/log.rs
+++ src/log.rs
@@ -12,11 +12,12 @@ impl Write for DBG {
         // Dunno why the copy is required, might be some pic issue as this is going straight to
         // assembly.
         for c in s.chars() {
-            use ledger_device_sdk::log;
+            use ledger_device_sdk::{log, testing::debug_print};
 
             let mut qq = ArrayString::<1>::new();
             qq.push(c);
             log::debug!("{}", qq.as_str());
+            debug_print(s);
         }
         Ok(())
     }
